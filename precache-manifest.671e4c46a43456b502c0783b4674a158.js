e683cdc765765a4a3f513bcf1ceabbd9d543deec
L3oneMclear <apoWebDevAcc@gmail.com>
2019-11-27 01:21:39 +0200

    Updates

---
+++ precache-manifest.671e4c46a43456b502c0783b4674a158.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f099bfd4219f911021c8ffb95a7606e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "dc1d81ab140ea4152e8a",
+    "url": "/monsters-rolodex/static/css/main.2de2ecd0.chunk.css"
+  },
+  {
+    "revision": "072144937a437c299fb0",
+    "url": "/monsters-rolodex/static/js/2.12dc0d94.chunk.js"
+  },
+  {
+    "revision": "dc1d81ab140ea4152e8a",
+    "url": "/monsters-rolodex/static/js/main.4563d472.chunk.js"
+  },
+  {
+    "revision": "ec765bb5ff3b7357d49a",
+    "url": "/monsters-rolodex/static/js/runtime-main.db675a29.js"
+  }
+]);
